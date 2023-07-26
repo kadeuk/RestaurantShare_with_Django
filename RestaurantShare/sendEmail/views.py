f0fe05f184c3a695ca54e6cdb052e0dbec053048
kadeuk <gomsonjangin@gmail.com>
2023-07-26 15:31:57 +0900

    url&tempates setting finish

--- RestaurantShare/sendEmail/views.py
+++ RestaurantShare/sendEmail/views.py
@@ -1,3 +1,10 @@
+# shareEmail > views.py
+
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def sendEmail(request):
+    return HttpResponse('sendEmail')
+
