3ca1920dbea821998161b240be443c8ed4dfa3b5
kadeuk <gomsonjangin@gmail.com>
2023-07-28 13:20:53 +0900

    Add: finished homework

--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -3,23 +3,74 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import *
+
 # Create your views here.
 
 def index(request):
     #return HttpResponse('index')
     categories = Category.objects.all()
+    restaurants = Restaurant.objects.all()
     content = {
-        'categories': categories
+        'categories': categories, 'restaurants': restaurants
     }
     return render(request, 'shareRes/index.html', content)
 
-def restaurantDetail(request):
+def restaurantDetail(request,res_id):
     #return HttpResponse('restaurantDetail')
-    return render(request, 'shareRes/restaurantDetail.html')
+    restaurant = Restaurant.objects.get(id = res_id)
+    content = { 'restaurant': restaurant}
+    return render(request, 'shareRes/restaurantDetail.html', content)
 
 def restaurantCreate(request):
     #return HttpResponse('restaurantCreate')
-    return render(request, 'shareRes/restaurantCreate.html')
+    categories = Category.objects.all()
+    content = {'categories': categories}
+    #return render(request, 'shareRes/restaurantCreate.html')
+    return render(request, 'shareRes/restaurantCreate.html', content)
+
+def restaurantUpdate(request,res_id):
+    # return HttpResponse('restaurant를 수정할 페이지')
+    categories = Category.objects.all()
+    restaurant = Restaurant.objects.get(id = res_id)
+    content = {'categories': categories, 'restaurant': restaurant}
+    return render(request, 'shareRes/restaurantUpdate.html', content)
+
+def Delete_restaurant(request):
+    res_id = request.POST['resId']
+    restaurant = Restaurant.objects.get(id = res_id)
+    restaurant.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+def Update_restaurant(request):
+    resId = request.POST['resId']
+    change_category_id = request.POST['resCategory']
+    change_category = Category.objects.get(id = change_category_id)
+    change_name= request.POST['resTitle']
+    change_link = request.POST['resLink']
+    change_content = request.POST['resContent']
+    change_keyword = request.POST['resLoc']
+    before_reataurant = Restaurant.objects.get(id = resId)
+    before_reataurant.category = change_category
+    before_reataurant.restaurant_name = change_name
+    before_reataurant.restaurant_link = change_link
+    before_reataurant.restaurant_content = change_content
+    before_reataurant.restaurant_keyword = change_keyword
+    before_reataurant.save()
+    return HttpResponseRedirect(reverse('resDetailPage', kwargs={'res_id':resId}))
+
+
+
+
+def Create_restaurant(request):
+    category_id = request.POST['resCategory']
+    category = Category.objects.get(id = category_id)
+    name = request.POST['resTitle']
+    link = request.POST['resLink']
+    content = request.POST['resContent']
+    keyword = request.POST['resLoc']
+    new_res = Restaurant(category = category, restaurant_name=name, restaurant_link=link, restaurant_content=content, restaurant_keyword=keyword)
+    new_res.save()
+    return HttpResponseRedirect(reverse('index'))
 
 def categoryCreate(request):
     #return HttpResponse("categoryCreate")
